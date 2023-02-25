bc74dc8d2d5c504834c3358d751ab9ef44e109d9
samuelsenerwa <samuelsenerwa@gmail.com>
2023-02-25 13:33:18 -0800

    added a task

---
+++ 100-shell_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * Swap_ints: swap two integers in an array
+ * @a: The first integer to swap
+ * @b: The second integer to swap
+ *
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - sort an array of integers in ascending in order using
+ * the alogorithm
+ * @arry: An array of integers
+ * @size: The size of an array
+ *
+ * Description: Uses the Knuth interval sequence
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 3)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for(; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = 1;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -=gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
